10b844c6bf696570d8907bfe7013f98ad2dba5f3
Lucas Fortes <adef08@gmail.com>
2018-07-06 12:35:25 -0400

    Mensagem qualquer

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Comentario de teste.
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
